4daf24eca8dfe9b2c66058d71eb18b30c8687e8c
John Allen <john@threedogconsulting.com>
2023-02-08 15:42:10 -0500

    feat: read weatherapi token from config (#9)
    At this point token must be manually created. See #8

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+pub mod conditions;
+pub mod icons;
+pub mod location;
+
+const APP_NAME: &str = "conditions";
+const CONFIG_NAME: &str = "config";
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct Config {
+    weather_api_token: String,
+}
+
+pub fn run() {
+    let config: Config = confy::load(APP_NAME, CONFIG_NAME).unwrap();
+    let weatherapi_token = config.weather_api_token;
+    let location = location::current().unwrap();
+
+    let mut conditions =
+        conditions::current(&weatherapi_token, &location).unwrap();
+
+    let time_of_day = match conditions.is_day {
+        true => icons::TimeOfDay::Day,
+        _ => icons::TimeOfDay::Night,
+    };
+
+    conditions.set_icon(icons::icon_for(time_of_day, conditions.code));
+
+    println!("{}", ureq::serde_json::to_string(&conditions).unwrap());
+}
