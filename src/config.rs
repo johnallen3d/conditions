824115791f97e060fdd189bf6c05e761eadd114a
John Allen <john@threedogconsulting.com>
2023-02-10 16:16:51 -0500

    refactor(config): move to module
    Moves interactions with the `Config` struct into a module and attaches
    functions to the struct.

---
+++ src/config.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+
+pub const APP_NAME: &str = "conditions";
+pub const CONFIG_NAME: &str = "config";
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct Config {
+    pub location: String,
+    pub weatherapi_token: String,
+}
+
+impl Config {
+    pub fn load() -> Self {
+        confy::load(APP_NAME, CONFIG_NAME).unwrap()
+    }
+
+    pub fn set_location(location: &str) {
+        let mut config = Self::load();
+
+        config.location = location.to_owned();
+        config.store();
+
+        print!("location stored successfully");
+    }
+
+    pub fn set_weatherapi_token(token: &str) {
+        let mut config = Self::load();
+
+        config.weatherapi_token = token.to_owned();
+        config.store();
+
+        print!("weatherapi.com token stored successfully");
+    }
+
+    pub fn store(&self) {
+        confy::store(APP_NAME, CONFIG_NAME, self).unwrap();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,13 @@
 use clap::Parser;
-use serde::{Deserialize, Serialize};
 
 use args::*;
+use config::Config;
 
 mod args;
 pub mod conditions;
+mod config;
 pub mod icons;
 pub mod location;
-
-const APP_NAME: &str = "conditions";
-const CONFIG_NAME: &str = "config";
-
-#[derive(Deserialize, Serialize, Debug, Default)]
-struct Config {
-    weatherapi_token: String,
-    location: String,
-}
 
 pub fn run() {
     let args = ConditionsArgs::parse();
@@ -24,23 +16,21 @@ pub fn run() {
         Command::Current => current_conditions(),
         Command::Location(cmd) => match &cmd.command {
             LocationSubcommand::Set(location) => {
-                set_location(&location.location);
+                Config::set_location(&location.location);
             }
             LocationSubcommand::View => view_location(),
         },
         Command::Token(cmd) => match &cmd.command {
-            TokenSubcommand::Set(token) => set_weatherapi_token(&token.token),
+            TokenSubcommand::Set(token) => {
+                Config::set_weatherapi_token(&token.token)
+            }
             TokenSubcommand::View => view_weatherapi_token(),
         },
     }
 }
 
-fn load_config() -> Config {
-    confy::load(APP_NAME, CONFIG_NAME).unwrap()
-}
-
 fn current_conditions() {
-    let config = load_config();
+    let config = Config::load();
 
     let location = if config.location.is_empty() {
         location::current().unwrap().to_string()
@@ -63,34 +53,14 @@ fn current_conditions() {
     println!("{}", ureq::serde_json::to_string(&conditions).unwrap());
 }
 
-fn set_location(location: &str) {
-    let mut config = load_config();
-
-    config.location = location.to_owned();
-
-    confy::store(APP_NAME, CONFIG_NAME, config).unwrap();
-
-    print!("location stored successfully");
-}
-
 fn view_location() {
-    let config = load_config();
+    let config = Config::load();
 
     println!("{}", config.location);
 }
 
-fn set_weatherapi_token(token: &str) {
-    let mut config = load_config();
-
-    config.weatherapi_token = token.to_owned();
-
-    confy::store(APP_NAME, CONFIG_NAME, config).unwrap();
-
-    print!("weatherapi.com token stored successfully");
-}
-
 fn view_weatherapi_token() {
-    let config: Config = confy::load(APP_NAME, CONFIG_NAME).unwrap();
+    let config = Config::load();
 
     println!("{}", config.weatherapi_token);
 }
